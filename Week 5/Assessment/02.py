db89c4e2150539058649f7921549a9781c6cddc9
Jlobblet <37539527+Jlobblet@users.noreply.github.com>
2019-02-08 23:26:49 +0000

    Update 02.py
    this is the fully-working version

--- Week 5/Assessment/02.py
+++ Week 5/Assessment/02.py
@@ -1,14 +1,21 @@
 import numpy as np
 
 def lorentz_test(mat):
-    try:
-        if mat.shape[0] != mat.shape[1] or np.linalg.det(mat) != 1: return False
-        minowski = np.matrix(np.array(((np.array([1,0,0,0]),np.array([0,-1,0,0]),np.array([0,0,0,-1]),np.array([0,0,0,-1])))))
-        at = np.transpose(mat)
-        test = np.matmul(at,np.matmul(minowski,mat))
-        if test[0][0] <= 0.99 or test[0][0] >= 1.01 or test[1][1] <= -1.01 or test[1][1] >= -0.99 or test[2][2] <= -1.01 or test[2][2] >= -0.99 or test[3][3] <= -1.01 or test[3][3] >= -0.99: return False 
-        return True
-    except:
-        return False
-
-print(lorentz_test(np.matrix(np.array(((np.array([1,0,0,0]),np.array([0,-1,0,0]),np.array([0,0,0,-1]),np.array([0,0,0,-1])))))))
+	try:
+		if mat.shape[0] != mat.shape[1] or not np.isclose(1,np.linalg.det(mat),rtol=0.01): return False
+		m = list(())
+		for i in range(len(mat)):
+			r = list(())	
+			for j in range(len(mat)):
+				if i == j:
+					if i == 0: r.append(-1)
+					else: r.append(1)
+				else: r.append(0)
+			m.append(r)
+		minkowski = np.mat(m)
+		at = np.transpose(mat)
+		test = np.matmul(at,np.matmul(minkowski,mat))
+		if np.allclose(minkowski,test,rtol=0.01,atol=0.01): return True
+		else: return False
+	except:
+		return False
